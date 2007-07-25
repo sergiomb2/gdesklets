f26934ddd8cb5f705cf96561449540a07df41a7c
Christian Meyer <chrisime@speedy>
2007-07-25 12:06:16 +0200

    Removed egg code. gtk has statusicon since v2.10.

--- main/TrayIcon.py
+++ main/TrayIcon.py
@@ -1,19 +1,5 @@
 from main import ICON
 
-try:
-    from utils.systray import Icon as SysTrayIcon
-except ImportError:
-    import sys
-    log("Could not import systray module!")
-    sys.exit(1)
-
-try:
-    from utils.tiling import Tiling
-except ImportError:
-    import sys
-    log("Could not import tiling module!")
-    sys.exit(1)
-
 import gtk
 
 
@@ -23,25 +9,17 @@ class TrayIcon:
 
     def __init__(self):
 
-        icon = Tiling()
-        self.__ebox = gtk.EventBox()
         self.__menu = gtk.Menu()
-        self.__trayicon = SysTrayIcon("gDesklets")
 
-        icon.set_from_file(ICON);
-        icon.render(24, 24, 1, 1);
-
-        self.__ebox.add(icon)
-        self.__ebox.show_all()
-        self.__trayicon.add(self.__ebox)
-
-        self.__ebox.connect("button-press-event", self.__on_button)
+        self.__trayicon = gtk.status_icon_new_from_file(ICON)
+        self.__trayicon.connect("popup-menu", self.__on_button)
 
 
-    def __on_button(self, src, event):
 
-        if (event.button == 3):
-            self.__menu.popup(None, None, None, event.button, event.time)
+    def __on_button(self, widget, button, time):
+
+        if (button == 3):
+            self.__menu.popup(None, None, None, button, time)
 
 
     def set_menu(self, items):
@@ -64,7 +42,3 @@ class TrayIcon:
             item.show()
             self.__menu.append(item)
 
-
-    def connect(self, event, *args):
-
-        self.__ebox.connect(event, *args)
