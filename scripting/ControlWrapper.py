d0b324f7a40816b89dfd9ac4fbdd6322d5fa21de
Joe Sapp <nixphoeni@gmail.com>
2009-12-13 22:44:17 -0500

    Implemented addition and deletion of Control array elements and implemented access to the elements in a more robust way, I believe

--- scripting/ControlWrapper.py
+++ scripting/ControlWrapper.py
@@ -11,9 +11,8 @@ class ControlWrapper(object):
 
     def __init__(self, control, size=0):
 
-        # Don't think it matters if the sandbox can see the length of this (in
-        # fact, isn't that what's desired?)
-        self.__dict__["length"] = size
+        self.__dict__["_ControlWrapper__length"] = size
+        # can we just use the 'size' variable?
         array_size = size
         if size == 0:
             array_size = 1
@@ -23,9 +22,13 @@ class ControlWrapper(object):
         self.__dict__["_ControlWrapper__control"] = \
                      Vault( [ deepcopy(control)
                               for i in range(array_size) ] )
+        # Keep a an original copy around for extending the array
+        self.__dict__["_ControlWrapper__original_control"] = Vault(control)
+        # This creates a dictionary of property arrays accesible by the keys
+        # normally used to access the property
         self.__dict__["_ControlWrapper__properties"] = \
-                     dict([ (k, PropertyArray(v, self.__dict__["_ControlWrapper__control"], array_size))
-                                   for (k, v) in Interface.get_properties(control.__class__) ])
+                     dict([ (k, PropertyArray(v, self.__control, array_size))
+                                    for (k, v) in Interface.get_properties(control.__class__) ])
 
         ids =  [ Interface.get_id(i)
                  for i in Interface.get_interfaces( control.__class__ ) ]
@@ -37,19 +40,45 @@ class ControlWrapper(object):
 
 
 
+    def __len__(self):
+
+        return self.__length
+
+
+
     def __setattr__(self, name, value):
 
         if name == "length":
-            if self.__dict__["length"] == 0:
-                self.__dict__["length"] = 1
+            # A little bounds checking
+            if value <= 0:
+              log("Warning: Value of property \"length\" must be greater than 0 (setting to 1)")
+              value = 1
 
-            while value > self.__dict__["length"]:
-                #TODO: implement deletion
-                self.__control(open)[name].append(self.__control(open)[0])
-                self.__properties(open)[name].append(self.__properties(open)[name][0])
-                self.__dict__["length"] += 1
+            # Don't do anything if value isn't changing
+            if value != self.__length:
+                if value > self.__length:
+                    self.__dict__["_ControlWrapper__control"] = \
+                        Vault( self.__control(open) +           \
+                               [ deepcopy(self.__original_control(open))  \
+                                 for i in range(self.__length, value) ] )
+                elif value < self.__length:
+                    # We want to leave the "0th" item alone
+                    # Handled by the above conditionals
+                    start_deleting_at = value #if value != 0 else 1
+                    c = self.__control(open)
+                    for i in range(start_deleting_at, self.__length):
+                        del c[i]
+                    self.__dict__["_ControlWrapper__control"] = Vault(c)
+
+                # Pass on the value to each item in the PropertyArray
+                for k in self.__properties.keys():
+                    # Reset the PropertyArray's control element
+                    self.__properties[k]._set_control(self.__control, open)
+                    self.__properties[k]._set_length(value, open)
+                self.__dict__["_ControlWrapper__length"] = value
         else: # name != "length"
-            if self.__dict__["length"] == 0:
+            # Backwards compatibility
+            if self.__length == 1:
                 try:
                     prop = self.__properties[name][0]
                 except KeyError:
@@ -57,36 +86,34 @@ class ControlWrapper(object):
                     return
 
                 prop.fset(self.__control(open)[0], value)
-            else: # length != 0
+            # This is the case where someone tries to access a property
+            # of this class when the length != 1. They should know
+            # better if they've gone and changed the length, but we'll
+            # be nice and print out some informational warnings.
+            else: # length != 1
                 try:
-                    self.__properties[name]
+                    prop = self.__properties[name]
                 except KeyError:
                     log("Warning: Property \"%s\" isn't available." % (name,))
+                finally:
+                    log("Warning: Property \"%s\" must be indexed (length != 0)." % (name,))
                     return
-
-                log("Warning: Property \"%s\" must be indexed." % (name,))
-                return
 
 
 
     def __getattr__(self, name):
 
         if name in Control.AUTHORIZED_METHODS:
-            if self.__dict__["length"] == 0:
+            if self.__length == 0:
                 return getattr(self.__control(open)[0], name)
             else:
                 return self.__control(open)
 
+        # This is where the magic happens.
+        # The __properties dict has its own locking mechanisms, so
+        # just give the whole thing to the user.
         if name == "length":
-            return self.length
-        elif self.__dict__["length"] == 0:
-            try:
-                prop = self.__properties[name][0]
-            except KeyError:
-                log("Warning: Property \"%s\" isn't available." % (name,))
-                return None
-
-            return prop.fget(self.__control(open)[0])
+            return self.__length
         else:
             return self.__properties[name]
 
--- scripting/PropertyArray.py
+++ scripting/PropertyArray.py
@@ -1,20 +1,75 @@
+from Vault import Vault
+
 class PropertyArray():
 
-    def __init__(self, prop, ctrl, length):
+    # like Vault...open isn't available from the sandbox
 
+    def __init__(self, prop, ctrl, length, key = open):
+
+        self.__dict__["_PropertyArray__key"] = Vault(key)
         self.__dict__["_PropertyArray__property"] = Vault([ prop for i in range(length) ])
-        self.__dict__["_PropertyArray__control"] = ctrl # already locked in Vault
+        self.__dict__["_PropertyArray__original_property"] = Vault(prop)
         self.__dict__["_PropertyArray__length"] = Vault(length)
+        #self.__dict__["_PropertyArray__control"] = ctrl # already locked in Vault
+        self._set_control(ctrl, open)
 
 
 
     def __len__(self):
 
-        return self.__dict__["_PropertyArray__length"](open)
+        return self.__length(open)
+
+
+
+    def _set_control(self, new_ctrl, k):
+
+        if k != self.__key(open):
+            raise RuntimeError("Intrusion detected (in PropertyArray %s)" % self)
+
+        self.__dict__["_PropertyArray__control"] = new_ctrl # already locked in Vault
+
+
+
+    def _set_length(self, value, k):
+
+        if k != self.__key(open):
+            raise RuntimeError("Intrusion detected (in PropertyArray %s)" % self)
+
+        length = self.__length(open)
+        if value <= 0:
+            log("Warning: Value of property \"length\" must be greater than 0 (setting to 1)")
+            value = 1
+
+        if value != length:
+            #print "debug: old length: %d" % length
+            #print "debug: new length: %d" % value
+            if value > length:
+                self.__dict__["_PropertyArray__property"] = \
+                    Vault( self.__property(open) + \
+                           [ self.__original_property(open)
+                             for i in range(length, value) ] )
+                #print "debug: added items %s to PropertyArray" % range(length, value)
+            elif value < length:
+                # We want to leave the 0th item alone
+                # This is checked by the above conditionals
+                start_deleting_at = value
+
+                prop = self.__property(open)
+                for i in range(start_deleting_at, length):
+                    #print "debug: deleting item %d" % i
+                    del prop[i]
+                self.__dict__["_PropertyArray__property"] = Vault(prop)
+
+            self.__dict__["_PropertyArray__length"] = Vault(value)
 
 
 
     def __setitem__(self, key, value):
+
+        if key < 0:
+            key = self.__length(open) + key
+        if key >= self.__length(open):
+            raise IndexError
 
         try:
             prop = self.__property(open)[key]
@@ -28,7 +83,9 @@ class PropertyArray():
 
     def __getitem__(self, key):
 
-        if key <= (0 - self.__length(open)) or key >= self.__length(open):
+        if key < 0:
+            key = self.__length(open) + key
+        if key >= self.__length(open):
             raise IndexError
 
         try:
@@ -38,23 +95,3 @@ class PropertyArray():
             return None
 
         return prop.fget(self.__control(open)[key])
-
-
-    def __delitem__(self, key):
-
-        if key <= (0 - self.__length(open)) or key >= self.__length(open):
-            raise IndexError
-
-        prop = self.__property(open)
-        length = self.__length(open)
-
-        try:
-            del prop[key]
-        except KeyError:
-            log("Warning: Property isn't available at index %d." % (key,))
-            raise KeyError
-
-        length -= 1
-
-        self.__property = Vault(prop)
-        self.__length = Vault(length)
