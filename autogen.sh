b61093f1c9b0291eedcedd64f048948e81eb4a4c
Joe Sapp <nixphoeni@gmail.com>
2010-07-25 18:59:54 -0400

    Now we need to test for configure.ac instead of configure.in in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="gDesklets"
 ACLOCAL_FLAGS="-I macros $ACLOCAL_FLAGS"
 
-(test -f $srcdir/configure.in) || {
+(test -f "$srcdir/configure.ac") || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
